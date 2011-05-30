1f3e36419f69fb769b1e523f60358c9f0efefa05
Tung Nguyen <tung.berlin@gmail.com>
2011-05-30 16:15:23 +0200

    Done with sign in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ def title
     end
 end
 
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Sampleapp::Application.routes.draw do
 
   
+ # get "sessions/new"
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
  #get "pages/home"
 
@@ -15,6 +17,8 @@ Sampleapp::Application.routes.draw do
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
     match '/signup',  :to => 'users#new'
+    match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
     root :to => 'pages#home'
 
