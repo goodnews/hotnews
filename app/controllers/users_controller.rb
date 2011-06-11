aba5a402562b2074621e7db5cec4ddba5711a2ad
Tung Nguyen <tung.berlin@gmail.com>
2011-06-12 00:10:18 +0200

    chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
   def show
     @user = User.find(params[:id])
     @title = @user.name
@@ -11,6 +12,7 @@ class UsersController < ApplicationController
   end
   
   def create
+    
     @user = User.new(params[:user])
     if @user.save
       # Handle a successful save.
@@ -22,5 +24,33 @@ class UsersController < ApplicationController
     end
     
   end
+  
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+    
+  end
+  
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
+    
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 
 end
